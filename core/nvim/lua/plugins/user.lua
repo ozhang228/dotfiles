7ff75d4c7cda27fd87df24b665f3360d42524c39
Oscar Zhang <oscarzhang228@gmail.com>
2025-04-27 12:15:57 -0400

    add obsidian to windows only

--- core/nvim/lua/plugins/user.lua
+++ core/nvim/lua/plugins/user.lua
@@ -104,32 +104,4 @@ return {
     "kevinhwang91/nvim-bqf",
     opts = {},
   },
-
-  {
-    "obsidian-nvim/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "Saghen/blink.cmp",
-      "folke/snacks.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "Career Notes",
-          path = "G:/My Drive/Career Notes",
-        },
-        {
-          name = "Archive",
-          path = "G:/My Drive/Archive",
-        },
-      },
-      completion = {
-        blink = true,
-        nvim_cmp = false,
-      },
-    },
-  },
 }
---
+++ core/nvim/lua/plugins/windows.lua
@@ -0,0 +1,29 @@
+if vim.fn.has "win32" then
+  return {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "Saghen/blink.cmp",
+      "folke/snacks.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Career Notes",
+          path = "G:/My Drive/Career Notes",
+        },
+        {
+          name = "Archive",
+          path = "G:/My Drive/Archive",
+        },
+      },
+      completion = {
+        blink = true,
+        nvim_cmp = false,
+      },
+    },
+  }
+end
