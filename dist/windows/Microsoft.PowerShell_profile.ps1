46970739649df8e9319d70e58b9aa1a9455b414b
Oscar Zhang <oscarzhang228@gmail.com>
2025-04-28 11:12:56 -0400

    make powershell not flash on vi mode

--- dist/windows/Microsoft.PowerShell_profile.ps1
+++ dist/windows/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,30 @@
 # Starship 
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\.config\Starship\starship.toml"
-
 # Zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+
 # turn file directory highlighting off
 $PSStyle.FileInfo.Directory = ""
-
 # set <C-t> as the keybinding for fzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
-
 # Put Vi Mode on
-Set-PSREadlineOption -EditMode Vi
+function OnViModeChange
+{
+  if ($args[0] -eq 'Command')
+  {
+    Write-Host -NoNewLine "`e[2 q"
+  } else
+  {
+    Write-Host -NoNewLine "`e[6 q"
+  }
+}
+$PSReadLineOptions = @{
+  EditMode = "vi"
+  HistoryNoDuplicates = $true
+  HistorySearchCursorMovesToEnd = $true
+  ViModeIndicator = "Script"
+  ViModeChangeHandler = $Function:OnViModeChange
+}
+Set-PSReadLineOption @PSReadLineOptions
