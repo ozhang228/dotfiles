c1fe97795be83ea889336ec24086f0ec45a8c6f0
Oscar Zhang <oscarzhang228@gmail.com>
2025-04-28 21:49:47 -0400

    add recording status

--- core/nvim/lua/plugins/astroui.lua
+++ core/nvim/lua/plugins/astroui.lua
@@ -75,7 +75,13 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diagnostics" },
-        lualine_c = { "filename" },
+        lualine_c = {
+          { "filename" },
+          {
+            require("noice").api.status.mode.get,
+            cond = require("noice").api.status.mode.has,
+          },
+        },
         lualine_x = { "diff" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
