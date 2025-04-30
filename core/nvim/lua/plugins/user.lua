9f9ffb8d7774ffbb2ede877c97a5e46267bbbccb
Oscar Zhang <oscarzhang228@gmail.com>
2025-04-30 13:58:05 -0400

    wrap errors in trouble nvim

--- core/nvim/lua/plugins/user.lua
+++ core/nvim/lua/plugins/user.lua
@@ -56,7 +56,13 @@ return {
   },
   {
     "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {
+      win = {
+        wo = {
+          wrap = true,
+        },
+      },
+    },
     cmd = "Trouble",
     keys = {
       {
