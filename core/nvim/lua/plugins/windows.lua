0eaf889e6ba69ca60d079fb5a1a9a05d97d9f13d
Oscar Zhang <oscarzhang228@gmail.com>
2025-06-17 21:06:34 -0500

    add tab control with numbers in kitty conf

--- core/nvim/lua/plugins/windows.lua
+++ core/nvim/lua/plugins/windows.lua
@@ -1,34 +1,26 @@
-if vim.fn.has "win32" == 1 then
-  return {
-    "obsidian-nvim/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "Saghen/blink.cmp",
-      "folke/snacks.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "Career Notes",
-          path = "G:/My Drive/Career Notes",
-        },
-        {
-          name = "Archive",
-          path = "G:/My Drive/Archive",
-        },
-      },
-      completion = {
-        blink = true,
-        nvim_cmp = false,
-      },
-      ui = {
-        enable = false,
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "Saghen/blink.cmp",
+    "folke/snacks.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Notes",
+        path = "$HOME/notes",
       },
     },
-  }
-else
-  return {}
-end
+    completion = {
+      blink = true,
+      nvim_cmp = false,
+    },
+    ui = {
+      enable = false,
+    },
+  },
+}
