0498fbb356f6b9d3a99f0901914761a5fe564680
Oscar Zhang <oscarzhang228@gmail.com>
2026-03-26 11:56:33 -0500

    lsp var colorscheme

--- nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/lua/plugins/ui/colorscheme.lua
@@ -4,6 +4,7 @@ return {
   opts = {
     variant = "moon",
     highlight_groups = {
+      ["@lsp.type.variable"] = { fg = "text" },
       LineNr = { fg = "iris" },
       NonText = { fg = "muted" },
     },
