f30eb702d3a5dd573330b1841da2fe5fe2373ad1
Oscar Zhang <oscarzhang228@gmail.com>
2026-03-29 13:05:27 -0500

    src

---
+++ src/terminal/shell/fish/conf.d/03-fastfetch.fish
@@ -0,0 +1,1 @@
+fastfetch --logo $HOME/dotfiles/imgs/conkeldurr_ascii.txt -c $HOME/dotfiles/src/terminal/fastfetch/fastfetch.jsonc 
--- terminal/shell/fish/conf.d/03-fastfetch.fish
+++
@@ -1,1 +0,0 @@
-fastfetch --logo $HOME/dotfiles/imgs/conkeldurr_ascii.txt -c $HOME/dotfiles/terminal/fastfetch/fastfetch.jsonc 
