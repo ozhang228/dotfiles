28c90cb82ccba9e22e46d0f394ed6b3fd0790f60
Oscar Zhang <oscarzhang228@gmail.com>
2026-04-03 20:30:27 -0500

    dap view

--- src/terminal/nvim/lua/plugins/dap/dap_view.lua
+++ src/terminal/nvim/lua/plugins/dap/dap_view.lua
@@ -4,6 +4,26 @@ return {
   opts = {
     winbar = {
       default_section = "scopes",
+      sections = { "scopes", "watches", "breakpoints", "threads", "repl", "sessions" },
+      base_sections = {
+        scopes = { label = "Scopes", keymap = "S" },
+        watches = { label = "Watches", keymap = "W" },
+        breakpoints = { label = "Breakpoints", keymap = "B" },
+        threads = { label = "Threads", keymap = "T" },
+        repl = { label = "REPL", keymap = "R" },
+        sessions = { label = "Sessions", keymap = "K" },
+      },
     },
+
+    windows = {
+      size = 0.5,
+      position = "right",
+      terminal = {
+        position = "right",
+        size = 0.5,
+      },
+    },
+
+    auto_toggle = "open_term",
   },
 }
