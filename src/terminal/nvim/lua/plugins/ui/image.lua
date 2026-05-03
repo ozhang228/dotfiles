1914d4b16be311d448a539ab7ba68d425f3f6cff
Oscar Zhang <oscarzhang228@gmail.com>
2026-05-02 22:28:55 -0500

    latex rendering

---
+++ src/terminal/nvim/lua/plugins/ui/image.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    image = {
+      enabled = true,
+      math = {
+        latex = {
+          font_size = "normalsize",
+        },
+      },
+    },
+  },
+}
