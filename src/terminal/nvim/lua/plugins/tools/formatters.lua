1f3271e2dedf9ad7fe617a47d17b0c77712f178e
Oscar Zhang <oscarzhang228@gmail.com>
2026-05-27 08:47:55 -0500

    remove prettier markdown format

--- src/terminal/nvim/lua/plugins/tools/formatters.lua
+++ src/terminal/nvim/lua/plugins/tools/formatters.lua
@@ -15,7 +15,6 @@ return {
       rust = { "rustfmt" },
       -- Misc
       json = { "prettierd", "prettier", stop_after_first = true },
-      markdown = { "prettierd", "prettier", stop_after_first = true },
     },
     format_on_save = {
       timeout_ms = 500,
