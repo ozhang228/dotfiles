798867dead9a9cdc2644cd731cde2da546abcdfe
Oscar Zhang <oscarzhang228@gmail.com>
2026-05-31 13:45:03 -0500

    tab fixes

--- src/terminal/nvim/lua/options.lua
+++ src/terminal/nvim/lua/options.lua
@@ -9,6 +9,8 @@ vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
 
 -- Sync clipboard between OS and Neovim.
 -- Over SSH, use OSC 52 so yanks land in the terminal emulator's clipboard.
