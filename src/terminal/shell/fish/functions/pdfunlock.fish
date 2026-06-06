80334302ffeafc4b75c0a0b63bf20052f2ffc08e
Oscar Zhang <oscarzhang228@gmail.com>
2026-06-06 15:03:36 -0500

    fix up

--- src/terminal/shell/fish/functions/pdfunlock.fish
+++ src/terminal/shell/fish/functions/pdfunlock.fish
@@ -23,26 +23,24 @@ function pdfunlock
         return 1
     end
     
-    read -s -p "Enter password: " pw
+    read -s -P "Enter password: " pw
     echo
-    
-    # Helper function
-    function _pdfunlock_one
-        set -l in "$argv[1]"
-        set -l out (string replace '.locked.pdf' '.pdf' $in)
-        qpdf --decrypt --password="$pw" -- "$in" "$out" && rm -f -- "$in"
+
+    function _pdfunlock_one --argument-names src password
+        set -l out (string replace -r '\.locked\.pdf$' '.pdf' $src)
+        qpdf --decrypt --password="$password" -- "$src" "$out" && rm -f -- "$src"
     end
-    
+
     if test -d "$argv[1]"
         for f in "$argv[1]"/*.locked.pdf
             echo "Unlocking: $f"
-            _pdfunlock_one "$f" || return 1
+            _pdfunlock_one "$f" "$pw" || return 1
         end
     else
         if not string match -q '*.locked.pdf' "$argv[1]"
             echo "Error: expected .locked.pdf"
             return 1
         end
-        _pdfunlock_one "$argv[1]"
+        _pdfunlock_one "$argv[1]" "$pw"
     end
 end
