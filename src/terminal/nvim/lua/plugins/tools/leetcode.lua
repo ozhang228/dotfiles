7e2501e103bb6d5ba3286eec1ee57771b430fe7e
Oscar Zhang <oscarzhang228@gmail.com>
2026-06-07 14:12:17 -0500

    leetcode keys

--- src/terminal/nvim/lua/plugins/tools/leetcode.lua
+++ src/terminal/nvim/lua/plugins/tools/leetcode.lua
@@ -1,3 +1,12 @@
+local function leet_keymaps(bufnr)
+  require("which-key").add({
+    { "<leader>r", group = "Leetcode", buffer = bufnr },
+    { "<leader>rp", "<cmd>Leet list<cr>", desc = "Problems", buffer = bufnr },
+    { "<leader>rr", "<cmd>Leet run<cr>", desc = "Run", buffer = bufnr },
+    { "<leader>rs", "<cmd>Leet submit<cr>", desc = "Submit", buffer = bufnr },
+  })
+end
+
 return {
   "kawre/leetcode.nvim",
   cmd = "Leet",
@@ -9,6 +18,17 @@ return {
     lang = "python",
     plugins = {
       non_standalone = true,
+    },
+    hooks = {
+      ---@type fun()[]
+      enter = {
+        -- the menu buffer is focused right after mounting, so grab it here
+        function() leet_keymaps(vim.api.nvim_get_current_buf()) end,
+      },
+      ---@type fun(question: lc.ui.Question)[]
+      question_enter = {
+        function(question) leet_keymaps(question.bufnr) end,
+      },
     },
   },
 }
