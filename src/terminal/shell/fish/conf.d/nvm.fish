9899dfc3ab26f9086283cd1b3adfca738785c4c5
Oscar Zhang <oscarzhang228@gmail.com>
2026-06-09 16:05:37 -0500

    silence

--- src/terminal/shell/fish/conf.d/nvm.fish
+++ src/terminal/shell/fish/conf.d/nvm.fish
@@ -1,4 +1,4 @@
 set -gx NVM_DIR "$HOME/.nvm"
 if test -s "$NVM_DIR/nvm.sh"
-    bass "source $NVM_DIR/nvm.sh && nvm use default"
+    bass "source $NVM_DIR/nvm.sh && nvm use default" >/dev/null 2>&1
 end
