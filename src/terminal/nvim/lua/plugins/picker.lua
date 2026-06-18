2f1f5dfa330e93c38bcf58e68eb5eb97019b5105
Oscar Zhang <oscarzhang228@gmail.com>
2026-06-18 11:13:36 -0500

    picker stuff

--- src/terminal/nvim/lua/plugins/picker.lua
+++ src/terminal/nvim/lua/plugins/picker.lua
@@ -6,6 +6,18 @@ return {
     picker = {
       enabled = true,
       hidden = true,
+      exclude = {
+        ".git",
+        "node_modules",
+        ".venv",
+        "__pycache__",
+        ".mypy_cache",
+        ".pytest_cache",
+        ".ruff_cache",
+        "dist",
+        "target",
+        "*.lock",
+      },
       win = {
         list = {
           wo = {
@@ -23,9 +35,7 @@ return {
       },
       transform = function(item)
         local name = item.file and vim.fn.fnamemodify(item.file, ":t") or item.text or ""
-        if name:sub(1, 1) == "_" then
-          item.score_add = (item.score_add or 0) - 20
-        end
+        if name:sub(1, 1) == "_" then item.score_add = (item.score_add or 0) - 20 end
       end,
     },
   },
