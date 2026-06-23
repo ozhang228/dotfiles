20fa115350505cd312ad9eed8b52f058bee22f10
Oscar Zhang <oscarzhang228@gmail.com>
2026-06-22 19:24:37 -0500

    better

--- src/terminal/nvim/lua/keymaps/git.lua
+++ src/terminal/nvim/lua/keymaps/git.lua
@@ -8,24 +8,14 @@ return {
       desc = "Lazygit",
     },
     {
-      "<leader>gha",
+      "<leader>gh",
       function() Snacks.picker.gh_pr() end,
       desc = "GH PRs (all open)",
     },
     {
-      "<leader>ghm",
+      "<leader>gm",
       function() Snacks.picker.gh_pr({ author = "@me" }) end,
       desc = "GH PRs (mine)",
-    },
-    {
-      "<leader>ght",
-      function() Snacks.picker.gh_pr({ label = "typescript" }) end,
-      desc = "GH PRs (typescript)",
-    },
-    {
-      "<leader>ghp",
-      function() Snacks.picker.gh_pr({ label = "python" }) end,
-      desc = "GH PRs (python)",
     },
   },
 }
