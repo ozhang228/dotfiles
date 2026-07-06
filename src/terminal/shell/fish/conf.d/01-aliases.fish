d0366bfbf1262176ec2e0ce2acc42bb985db08ee
Oscar Zhang <oscarzhang228@gmail.com>
2026-07-05 21:55:47 -0500

    uv marimo

--- src/terminal/shell/fish/conf.d/01-aliases.fish
+++ src/terminal/shell/fish/conf.d/01-aliases.fish
@@ -4,6 +4,7 @@ command -v lsd &>/dev/null && alias ls='lsd'
 
 abbr -a py "python3"
 abbr -a pm "sudo pacman"
+abbr -a marimo "uv run marimo"
 
 abbr -a dot "cd $HOME/dotfiles/;nvim"
 abbr -a forge "cd $HOME/forge;nvim"
