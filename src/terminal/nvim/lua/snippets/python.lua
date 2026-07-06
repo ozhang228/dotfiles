940aa4480d86848087454047fb0f6ee2002557f8
Oscar Zhang <oscarzhang228@gmail.com>
2026-07-05 21:38:49 -0500

    full

--- src/terminal/nvim/lua/snippets/python.lua
+++ src/terminal/nvim/lua/snippets/python.lua
@@ -5,7 +5,7 @@ local i = ls.insert_node
 
 return {
   s("marimonb", {
-    t({ "import marimo", "", "app = marimo.App()", "", "with app.setup:", "    " }),
+    t({ "import marimo", "", 'app = marimo.App(width="full")', "", "with app.setup:", "    " }),
     i(1, "pass"),
     t({ "", "", "", "@app.cell", "def " }),
     i(2, "_"),
@@ -14,11 +14,17 @@ return {
     t({ "", "    return", "", "", 'if __name__ == "__main__":', "    app.run()", "" }),
   }),
 
-  s("cell", {
+  s("@app.cell", {
     t({ "@app.cell", "def " }),
     i(1, "_"),
     t({ "():", "    " }),
     i(0),
     t({ "", "    return" }),
+  }),
+
+  s("mo.md", {
+    t({ 'mo.md("""', "" }),
+    i(1),
+    t({ "", '""")' }),
   }),
 }
