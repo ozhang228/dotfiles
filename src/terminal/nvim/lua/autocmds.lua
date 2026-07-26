23cf847c2c4e6eef7fec9260cc71e2830d7073f9
Oscar Zhang <oscarzhang228@gmail.com>
2026-07-26 12:39:13 -0500

    stuff

--- src/terminal/nvim/lua/autocmds.lua
+++ src/terminal/nvim/lua/autocmds.lua
@@ -40,6 +40,7 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     vim.opt_local.spell = true
     vim.opt_local.spelllang = "en_us"
+    vim.opt_local.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
   end,
 })
 
